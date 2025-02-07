096b426d7c7414912cfa61204d3d6bc6a6fc3c82
01vaibhavnaik <148302295+01vaibhavnaik@users.noreply.github.com>
2025-02-07 14:25:24 +0530

    Delete JavaProgs directory

--- JavaProgs/printInLine.java
+++
@@ -1,8 +0,0 @@
-class printInLine{
-    public static void main(String[] args) {
-        for (int i=0;i<=6;i++){
-            System.out.print("*");
-            // System.out.println("*");
-        }
-    }
-}
